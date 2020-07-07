07b226ae78bb8df0ad662cf2ac9ddb3060e983b1
fanghongliang <18292656094@163.com>
2020-07-07 14:43:32 +0800

    feat: 分析温度和海边距离关系

--- analyst/index.py
+++ analyst/index.py
@@ -83,8 +83,44 @@ plt.hist(a, bins=[0, 20, 40, 60, 80, 100])
 plt.title("histogram")
 plt.show()
 
-#
+# ---------------------------------------------------------------------
+# desc 不同城市海洋距离影响气温的图表
+# 读取温度和日期数据
+y1 = df_ravenna['temp']
+x1 = df_ravenna['day']
+y2 = df_faenza['temp']
+x2 = df_faenza['day']
+y3 = df_cesena['temp']
+x3 = df_cesena['day']
+y4 = df_milano['temp']
+x4 = df_milano['day']
+y5 = df_asti['temp']
+x5 = df_asti['day']
+y6 = df_torino['temp']
+x6 = df_torino['day']
+
+# 把日期从 string 类型转化为标准的 datetime 类型
+day_ravenna = [parser.parse(x) for x in x1]
+day_faenza = [parser.parse(x) for x in x2]
+day_cesena = [parser.parse(x) for x in x3]
+day_milano = [parser.parse(x) for x in x4]
+day_asti = [parser.parse(x) for x in x5]
+day_torino = [parser.parse(x) for x in x6]
+
+# 调用 subplots() 函数，重新定义 fig, ax 变量
+fig, ax = plt.subplots()
+plt.title('temp-sea')
+plt.xticks(rotation=70)
+
+hours = mdates.DateFormatter('%H:%M')
+ax.xaxis.set_major_formatter(hours)
+
+# 这里需要画出三根线，所以需要三组参数， 'g'代表'green'
+ax.plot(day_ravenna, y1, 'r', day_faenza, y2, 'r', day_cesena, y3, 'r')
+ax.plot(day_milano, y4, 'g', day_asti, y5, 'g', day_torino, y6, 'g')
+
+plt.show()
 
 
-
+# ----------------------------------------------------------------------
 
